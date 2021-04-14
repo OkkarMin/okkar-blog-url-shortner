eb3fc78b9e2caf3cb06a36b6eb9cfcdcbd09a860
Okkar Min <omin001@e.ntu.edu.sg>
2021-04-14 15:50:17 +0800

    🚀 Initial commit

---
+++ pages/[alias].js
@@ -0,0 +1,38 @@
+import redirect from "nextjs-redirect";
+import { Image, Page, Loading, Spacer } from "@geist-ui/react";
+
+export default function Alias(props) {
+  const { url, error } = props.data;
+  const Redirect = redirect(url);
+
+  if (error) {
+    return (
+      <Page>
+        <Image.Browser url={error} invert>
+          <Image width={600} height={290} src="/404.jpg" />
+        </Image.Browser>
+      </Page>
+    );
+  }
+
+  return (
+    <Page>
+      <Redirect>
+        <Loading>redirecting to {url}</Loading>
+        <Spacer />
+        <Image width={600} height={290} src="/redirect.png" />
+      </Redirect>
+    </Page>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const { alias } = query;
+
+  const res = await fetch(`http://localhost:3000/api/url/${alias}`);
+  const data = await res.json();
+
+  return {
+    props: { data },
+  };
+}
---
+++ pages/api/url/[alias].js
@@ -0,0 +1,30 @@
+import Cors from "cors";
+import initMiddleware from "../../../lib/init-middleware";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ["GET", "POST"],
+  })
+);
+
+export default async function handler(req, res) {
+  await cors(req, res);
+
+  // Rest of the API logic
+  const url_table = {
+    linkedin: "https://www.linkedin.com/in/okarmin/",
+    github: "https://github.com/OkkarMin",
+    telegram: "https://t.me/okarmax",
+  };
+
+  const { alias } = req.query;
+
+  if (!url_table.hasOwnProperty(alias)) {
+    return res.json({ error: `${alias} not found, did you mistype it?` });
+  }
+
+  return res.json({ url: url_table[alias] });
+}
