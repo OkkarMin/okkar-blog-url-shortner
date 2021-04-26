8ea3eea3cb963d1294a35ffdaeeb7da12a0c88ed
Okkar Min <omin001@e.ntu.edu.sg>
2021-04-26 21:31:29 +0800

    ✏️ Edit Home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,45 @@
-import AliasRedirect from "../components/alias_redirect";
+import { Box, Flex, Stack, Heading, Image } from "@chakra-ui/react";
 
 export default function Home() {
-  return <AliasRedirect to_url="https://okkarmin.github.io" />;
+  return (
+    <Flex
+      align="center"
+      justify={{ base: "center", md: "space-around", xl: "space-between" }}
+      direction={{ base: "column-reverse", md: "row" }}
+      wrap="no-wrap"
+      minH="70vh"
+      px={8}
+      mb={16}
+    >
+      <Stack
+        spacing={4}
+        w={{ base: "80%", md: "40%" }}
+        align={["center", "center", "flex-start", "flex-start"]}
+      >
+        <Heading
+          as="h1"
+          size="xl"
+          fontWeight="bold"
+          color="primary.800"
+          textAlign={["center", "center", "left", "left"]}
+        >
+          Hello
+        </Heading>
+        <Heading
+          as="h2"
+          size="md"
+          color="primary.800"
+          opacity="0.8"
+          fontWeight="normal"
+          lineHeight={1.5}
+          textAlign={["center", "center", "left", "left"]}
+        >
+          My name is Min, Okkar Min
+        </Heading>
+      </Stack>
+      <Box w={{ base: "80%", sm: "60%", md: "50%" }} mb={{ base: 12, md: 0 }}>
+        <Image src={"/redirect.png"} size="100%" rounded="1rem" shadow="2xl" />
+      </Box>
+    </Flex>
+  );
 }
