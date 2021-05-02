a8e26303a05d05b127ce0b23aa4bcb326939cd91
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-02 13:35:10 +0800

    ➕ Add firebase analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
+import { analytics } from "../lib/firebase";
+import { useRouter } from "next/router";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url);
+        analytics().logEvent("screen_view");
+      };
+
+      router.events.on("routeChangeComplete", logEvent);
+
+      // for first page
+      logEvent(window.location.pathname);
+
+      return () => router.events.off("routeChangeComplete", logEvent);
+    }
+  }, []);
+
   return (
     <ChakraProvider>
       <Component {...pageProps} />
