7ff5f71edcf0fcf88ffb17cf61bd150211643da7
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-04 21:03:28 +0800

    feat(introduction): :sparkles: Add introduction

---
+++ components/introduction.js
@@ -0,0 +1,40 @@
+import { server } from "../config";
+import NextLink from "next/Link";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { Stack, Heading, Text, Link } from "@chakra-ui/react";
+
+export default function Introduction() {
+  return (
+    <Stack
+      spacing={2}
+      direction="column"
+      justifyContent="flex-start"
+      alignItems="center"
+      px={[5]}
+    >
+      <Stack spacing={2} minWidth="60%">
+        <Heading>Hello 👋 I'm Okkar Min</Heading>
+
+        <Text>
+          Software Architect, System Engineer and Developer. Connect with me at{" "}
+          <Link color="cyan.700" isExternal={true} href={`${server}/linkedin`}>
+            LinkedIn
+            <ExternalLinkIcon mx="2px" />
+          </Link>
+        </Text>
+
+        <Text>
+          Read my{" "}
+          <NextLink href={`${server}/blog`}>
+            <Link color="cyan.700">blog posts </Link>
+          </NextLink>{" "}
+          or take a look at some of{" "}
+          <NextLink href={`${server}/projects`}>
+            <Link color="cyan.700">my projects </Link>
+          </NextLink>
+          while you're here.
+        </Text>
+      </Stack>
+    </Stack>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Layout from "../components/layout";
 import Header from "../components/header";
+import Introduction from "../components/introduction";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
+      <Introduction />
     </Layout>
   );
 }
