ba8b190261325b60451bb28333e3fedc50127d9b
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-04 20:00:52 +0800

    style(header): :lipstick: Add link to github and linkedin

--- components/header.js
+++ components/header.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { server } from "../config";
 import { Button, Stack, IconButton } from "@chakra-ui/react";
 import { AiFillGithub, AiFillLinkedin } from "react-icons/ai";
 
@@ -6,16 +8,24 @@ export default function Header() {
     <Stack spacing={2} justifyContent="flex-start" p={[2, 4, 6, 8]}>
       <Stack spacing={2} isInline justifyContent="space-around">
         <Stack spacing={2} isInline>
-          <IconButton variant="ghost" as={AiFillGithub} />
-          <IconButton variant="ghost" as={AiFillLinkedin} />
+          <Link href={`${server}/github`}>
+            <IconButton
+              aria-label="Link to Okkar's Github"
+              variant="ghost"
+              icon={<AiFillGithub />}
+            />
+          </Link>
+          <Link href={`${server}/linkedin`}>
+            <IconButton
+              aria-label="Link to Okkar's Linkedin"
+              variant="ghost"
+              icon={<AiFillLinkedin />}
+            />
+          </Link>
         </Stack>
         <Stack spacing={2} isInline>
-          <Button variant="ghost" size="md">
-            Blog
-          </Button>
-          <Button variant="ghost" size="md">
-            Resume
-          </Button>
+          <Button variant="ghost">Blog</Button>
+          <Button variant="ghost">Resume</Button>
         </Stack>
       </Stack>
       <Stack spacing={2} />
