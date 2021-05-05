e5c15821514b964b4992b38123153d1e7a3c049f
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-05 17:55:16 +0800

    feat(projects): :sparkles: Add projects page

---
+++ pages/projects.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+import Body from "../components/body";
+
+export default function Projects() {
+  return (
+    <Layout>
+      <Body />
+    </Layout>
+  );
+}
