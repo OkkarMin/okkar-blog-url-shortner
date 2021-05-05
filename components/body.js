f522f825805f0d08e6fd7fbcad11cfbf5b2e73af
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-05 12:44:08 +0800

    feat(body): :sparkles: Add body (content) of page

---
+++ components/body.js
@@ -0,0 +1,11 @@
+import { Container, Heading, Text, VStack } from "@chakra-ui/react";
+
+export default function Body() {
+  return (
+    <VStack mt={6}>
+      <Container centerContent>
+        <Heading size="sm">✏️ in progress</Heading>
+      </Container>
+    </VStack>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 import Layout from "../components/layout";
 import Header from "../components/header";
 import Introduction from "../components/introduction";
+import Body from "../components/body";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
     <Layout>
       <Header />
       <Introduction />
+      <Body />
+      <Footer />
     </Layout>
   );
 }
