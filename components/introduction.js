97db5b50bc061dda6ebb33f1ab39a6de4894fd5c
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-05 12:40:35 +0800

    style(introduction): :lipstick: Change to use 'Container' component

--- components/introduction.js
+++ components/introduction.js
@@ -1,49 +1,38 @@
 import { server } from "../config";
 import NextLink from "next/link";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
-import { Stack, Heading, Text, Link } from "@chakra-ui/react";
+import { Container, Heading, Text, Link } from "@chakra-ui/react";
 
 export default function Introduction() {
   return (
-    <Stack
-      spacing={2}
-      direction="column"
-      justifyContent="flex-start"
-      alignItems="center"
-      px={[5]}
-    >
-      <Stack spacing={2} minWidth="60%">
-        <Heading>Hello 👋 I'm Okkar Min</Heading>
+    <Container mt={6}>
+      <Heading>Hello 👋 I'm Okkar Min</Heading>
 
-        <Text>
-          Software Architect, System Engineer and Developer. Connect with me at{" "}
-          <Link
-            textDecoration="underline"
-            color="red.500"
-            isExternal={true}
-            href={`${server}/linkedin`}
-          >
-            LinkedIn
-            <ExternalLinkIcon mx="2px" />
+      <Text mt={6}>
+        Software Architect, System Engineer and Developer. Connect with me at{" "}
+        <Link
+          textDecoration="underline"
+          color="red.500"
+          isExternal={true}
+          href={`${server}/linkedin`}
+        >
+          LinkedIn
+          <ExternalLinkIcon mx="2px" />
+        </Link>
+        Read my{" "}
+        <NextLink href={`${server}/blog`}>
+          <Link textDecoration="underline" color="red.500">
+            blog posts
           </Link>
-        </Text>
-
-        <Text>
-          Read my{" "}
-          <NextLink href={`${server}/blog`}>
-            <Link textDecoration="underline" color="red.500">
-              blog posts
-            </Link>
-          </NextLink>{" "}
-          or take a look at some of{" "}
-          <NextLink href={`${server}/projects`}>
-            <Link textDecoration="underline" color="red.500">
-              my projects
-            </Link>
-          </NextLink>{" "}
-          while you're here.
-        </Text>
-      </Stack>
-    </Stack>
+        </NextLink>{" "}
+        or take a look at some of{" "}
+        <NextLink href={`${server}/projects`}>
+          <Link textDecoration="underline" color="red.500">
+            my projects
+          </Link>
+        </NextLink>{" "}
+        while you're here.
+      </Text>
+    </Container>
   );
 }
