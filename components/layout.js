c74a3e2b091f9fcee8093b4ed3837518b40572ed
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-05 12:46:04 +0800

    refactor(layout): Move header and footer to layout

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,14 @@
 import { Flex } from "@chakra-ui/react";
 
+import Header from "../components/header";
+import Footer from "../components/footer";
+
 export default function Layout({ children }) {
   return (
     <Flex direction="column" minHeight="100vh" p={[2, 4]}>
+      <Header />
       {children}
+      <Footer />
     </Flex>
   );
 }
