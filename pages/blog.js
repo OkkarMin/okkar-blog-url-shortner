73f9eb231f3f58ddce2e4dfdea7816c5171f8ba7
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-05 17:54:54 +0800

    feat(blog): :sparkles: Add blog page

---
+++ pages/blog.js
@@ -0,0 +1,10 @@
+import Layout from "../components/layout";
+import Body from "../components/body";
+
+export default function Blog() {
+  return (
+    <Layout>
+      <Body />
+    </Layout>
+  );
+}
