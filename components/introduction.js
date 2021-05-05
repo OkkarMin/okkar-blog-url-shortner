a7d5dea7cbef3daf89d0470108a6fe740e7f0776
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-05 09:11:05 +0800

    style(introduction): :lipstick: Add underline to links

--- components/introduction.js
+++ components/introduction.js
@@ -17,7 +17,12 @@ export default function Introduction() {
 
         <Text>
           Software Architect, System Engineer and Developer. Connect with me at{" "}
-          <Link color="red.500" isExternal={true} href={`${server}/linkedin`}>
+          <Link
+            textDecoration="underline"
+            color="red.500"
+            isExternal={true}
+            href={`${server}/linkedin`}
+          >
             LinkedIn
             <ExternalLinkIcon mx="2px" />
           </Link>
@@ -26,12 +31,16 @@ export default function Introduction() {
         <Text>
           Read my{" "}
           <NextLink href={`${server}/blog`}>
-            <Link color="red.500">blog posts </Link>
+            <Link textDecoration="underline" color="red.500">
+              blog posts
+            </Link>
           </NextLink>{" "}
           or take a look at some of{" "}
           <NextLink href={`${server}/projects`}>
-            <Link color="red.500">my projects </Link>
-          </NextLink>
+            <Link textDecoration="underline" color="red.500">
+              my projects
+            </Link>
+          </NextLink>{" "}
           while you're here.
         </Text>
       </Stack>
