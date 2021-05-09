037a5e08b70d9dd4737943767a9b673c0caa0ce7
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-09 13:37:46 +0800

    fix(redirect): :lipstick: Change VStack to Container in alias_error

--- components/alias_error.js
+++ components/alias_error.js
@@ -1,17 +1,17 @@
-import { Box, Flex, Heading, VStack } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
 import Image from "next/image";
 
 export default function AliasEror({ error_message }) {
   return (
     <Flex h="100vh" alignItems="center" justifyContent="center">
-      <VStack>
+      <Container>
         <Heading as="h3" size="lg">
           {error_message}
         </Heading>
         <Box m="20px auto" maxW="xl">
           <Image src="/404.jpg" width="600" height="400" />
         </Box>
-      </VStack>
+      </Container>
     </Flex>
   );
 }
