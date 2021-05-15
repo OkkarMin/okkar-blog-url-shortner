7c72d9663fed901fc90e47283694c16049136181
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-15 10:19:26 +0800

    refactor(blog): :recycle: Refactor blog_post_link code logic

--- components/blog_post_link.js
+++ components/blog_post_link.js
@@ -3,15 +3,19 @@ import { Link } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 export default function BlogPostLink({ as, href, ...otherProps }) {
-  const isInternalLink = href.indexOf("http") === -1 && href.indexOf("#") !== 0;
+  const isInternalLink = href && (href.startsWith("/") || href.startsWith("#"));
 
-  return isInternalLink ? (
-    <NextLink as={as} href={href}>
-      <Link color="red.500">
-        <a {...otherProps} />
-      </Link>
-    </NextLink>
-  ) : (
+  if (isInternalLink) {
+    return (
+      <NextLink as={as} href={href}>
+        <Link color="red.500">
+          <a {...otherProps} />
+        </Link>
+      </NextLink>
+    );
+  }
+
+  return (
     <Link color="red.500" href={href} isExternal>
       <a {...otherProps} /> <ExternalLinkIcon />
     </Link>
