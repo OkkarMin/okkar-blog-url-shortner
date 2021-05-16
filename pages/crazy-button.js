6ad036ff2535d4961dc88ef7c644107effe00a04
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-16 19:14:19 +0800

    feat: :sparkles: Crazy button working for multiplayer

--- pages/crazy-button.js
+++ pages/crazy-button.js
@@ -1,7 +1,8 @@
+import Head from "next/head";
+import { useRef, useState, useEffect } from "react";
 import { Box, Button } from "@chakra-ui/react";
-import { firestore } from "lib/firebase";
-import { useRef, useState } from "react";
 import useWindowDimensions from "hooks/useWindowDimensions";
+import { firestore } from "lib/firebase";
 
 export default function CrazyButtonPage() {
   const { height, width } = useWindowDimensions();
@@ -9,30 +10,59 @@ export default function CrazyButtonPage() {
   const [buttonLeft, setButtonLeft] = useState(0);
   const buttonRef = useRef(null);
 
+  useEffect(() => {
+    const unsubscribe = firestore
+      .collection("button")
+      .doc("buttonLocation")
+      .onSnapshot((doc) => {
+        moveToLocation(doc.data().randomTop, doc.data().randomLeft);
+      });
+
+    return function cleanup() {
+      unsubscribe();
+    };
+  }, []);
+
   function goCrazy() {
-    // Button maximum top and left position is window size - button size
+    const randomTop = Math.random();
+    const randomLeft = Math.random();
+
+    firestore.collection("button").doc("buttonLocation").set({
+      randomTop,
+      randomLeft,
+    });
+  }
+
+  function moveToLocation(randomTop, randomLeft) {
     const maxTop = height - buttonRef.current.clientHeight;
     const maxLeft = width - buttonRef.current.clientWidth;
 
     // + 1 becuase Math.random is [0, 1)
-    let newButtonTop = Math.random() * (maxTop + 1);
-    let newButtonLeft = Math.random() * (maxLeft + 1);
+    let newButtonTop = randomTop * (maxTop + 1);
+    let newButtonLeft = randomLeft * (maxLeft + 1);
 
+    // Update the button location
     setButtonTop(newButtonTop);
     setButtonLeft(newButtonLeft);
   }
 
   return (
-    <Box width="100vw" height="100vh">
-      <Button
-        colorScheme="red"
-        left={buttonLeft}
-        onClick={() => goCrazy()}
-        ref={buttonRef}
-        top={buttonTop}
-      >
-        Catch Me! 🤡
-      </Button>
-    </Box>
+    <>
+      <Head>
+        <title>🤪 Crazy Button | Okkar Min</title>
+      </Head>
+
+      <Box>
+        <Button
+          colorScheme="red"
+          left={buttonLeft}
+          onClick={() => goCrazy()}
+          ref={buttonRef}
+          top={buttonTop}
+        >
+          Catch Me! 🤡
+        </Button>
+      </Box>
+    </>
   );
 }
