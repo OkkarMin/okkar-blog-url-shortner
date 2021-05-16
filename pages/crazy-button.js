433de63da5f968cd5b7d91ce6caa7295a34fb9a1
Okkar Min <omin001@e.ntu.edu.sg>
2021-05-16 16:38:55 +0800

    chore: Emergency commit to disable write to firestore on button press

--- pages/crazy-button.js
+++ pages/crazy-button.js
@@ -1,20 +1,40 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { firestore } from "lib/firebase";
+import { useRef, useState } from "react";
+import useWindowDimensions from "hooks/useWindowDimensions";
 
 export default function CrazyButtonPage() {
+  const { height, width } = useWindowDimensions();
+  const buttonRef = useRef(null);
+
+  const [buttonTop, setButtonTop] = useState(0);
+  const [buttonLeft, setButtonLeft] = useState(0);
+
+  function goCrazy() {
+    const maxTop = height - buttonRef.current.clientHeight;
+    const maxLeft = width - buttonRef.current.clientWidth;
+
+    console.log(buttonRef.current.clientHeight);
+    console.log(buttonRef.current.clientWidth);
+
+    let newButtonTop = Math.random() * (maxTop + 1);
+    let newButtonLeft = Math.random() * (maxLeft + 1);
+
+    setButtonTop(newButtonTop);
+    setButtonLeft(newButtonLeft);
+  }
+
   return (
-    <Flex
-      direction="column"
-      height="100vh"
-      justifyContent="center"
-      alignItems="center"
-    >
+    <Box width="100vw" height="100vh">
       <Button
         colorScheme="red"
-        onClick={() => firestore.collection("users").add({ name: "Okkar" })}
+        left={buttonLeft}
+        onClick={() => goCrazy()}
+        ref={buttonRef}
+        top={buttonTop}
       >
-        Crazy Button
+        Catch Me!
       </Button>
-    </Flex>
+    </Box>
   );
 }
