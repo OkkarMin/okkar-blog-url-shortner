35cf5edc8d8f8dcdb0d891a251cbbd9ae30eb2e7
Okkar Min <OMIN001@e.ntu.edu.sg>
2021-10-27 14:19:48 +0800

    chore: :poop: Test react-scroll animations

---
+++ pages/about-me.js
@@ -0,0 +1,73 @@
+import { Heading, Text } from "@chakra-ui/react";
+
+const isServer = typeof window === "undefined";
+let RSS = null;
+try {
+  RSS = require("react-scroll-motion");
+} catch (error) {}
+
+export default function AboutMe() {
+  return !isServer ? (
+    <RSS.ScrollContainer>
+      <RSS.ScrollPage page={0}>
+        <RSS.Animator
+          animation={RSS.batch(RSS.Fade(), RSS.Sticky(), RSS.MoveOut(0, -200))}
+        >
+          <Heading>Let't me show you scroll animation 😀</Heading>
+        </RSS.Animator>
+      </RSS.ScrollPage>
+
+      <RSS.ScrollPage page={1}>
+        <RSS.Animator
+          animation={RSS.batch(RSS.StickyIn(), RSS.FadeIn(), RSS.ZoomIn())}
+        >
+          <Heading style={{ fontSize: "40px" }}>I'm FadeUpScrollOut ✨</Heading>
+        </RSS.Animator>
+      </RSS.ScrollPage>
+
+      <RSS.ScrollPage page={2}>
+        <RSS.Animator
+          animation={RSS.batch(RSS.Fade(), RSS.Move(), RSS.Sticky())}
+        >
+          <Heading>I'm FadeUp</Heading>
+        </RSS.Animator>
+      </RSS.ScrollPage>
+      <RSS.ScrollPage page={3}>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%",
+          }}
+        >
+          <span style={{ fontSize: "40px" }}>
+            <RSS.Animator animation={RSS.MoveIn(-1000, 0)}>
+              Hello Guys 👋🏻
+            </RSS.Animator>
+            <RSS.Animator animation={RSS.MoveIn(1000, 0)}>
+              Nice to meet you 🙋🏻‍♀️
+            </RSS.Animator>
+            <RSS.Animator animation={RSS.MoveOut(1000, 0)}>
+              Good bye ✋🏻
+            </RSS.Animator>
+            <RSS.Animator animation={RSS.MoveOut(-1000, 0)}>
+              See you 💛
+            </RSS.Animator>
+          </span>
+        </div>
+      </RSS.ScrollPage>
+
+      <RSS.ScrollPage page={4}>
+        <RSS.Animator animation={RSS.batch(RSS.Fade(), RSS.Sticky())}>
+          <Text>Done</Text>
+          <Text>
+            There's FadeAnimation, MoveAnimation, StickyAnimation, ZoomAnimation
+          </Text>
+        </RSS.Animator>
+      </RSS.ScrollPage>
+    </RSS.ScrollContainer>
+  ) : (
+    <div></div>
+  );
+}
